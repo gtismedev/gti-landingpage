ac7c9e47b4d3bc317405e09b5732c4395af1de3c
Hercules <hmls235@gmail.com>
2024-07-18 16:54:58 -0300

    feat: add hotfix branch

--- js/navbar/navbar.js
+++ js/navbar/navbar.js
@@ -1,5 +1,10 @@
 var menuButton = document.querySelector(".navbar__button-menu");
-var menu = document.querySelector(".navbar__menu-mobile");
+var menuMobile = document.querySelector(".navbar__menu-mobile");
+var menuMobileRect = document.querySelectorAll(
+  ".button-menu__svg-rect--closing"
+);
+var menuDesktop = document.querySelector(".navbar__menu-desktop");
+var navWidth = document.querySelector("#navbar-width");
 const menuItem = document.querySelectorAll(".menu-mobile__item");
 const buttonMenuSvgClosing = document.querySelector(
   "#button-menu__svg--closing"
@@ -11,13 +16,15 @@ const buttonMenuSvgOpen = document.querySelector("#button-menu__svg--open");
 
 //Função responsavel por abrir e fechar o menu-burguer
 menuButton.addEventListener("click", function () {
-  if (menu.style.display === "block") {
+  //Altera o menu para desktop
+  if (menuMobile.style.display === "block") {
+    //Define um "timer" de 750ms para que as animações rodem antes do display alterar para nones
     setTimeout(() => {
-      menu.style.display = "none";
+      menuMobile.style.display = "none";
     }, 750);
-    menu.style.animation = "menu-background-hide 0.8s linear";
-    menu.style.width = "0%";
-    //Mostra os itens do menu
+    menuMobile.style.animation = "menu-background-hide 0.8s linear";
+    menuMobile.style.width = "0%";
+    //Esconde os itens do menu mobile
     menuItem.forEach((item) => {
       item.style.animation =
         "menu-item-tracking-out 0.4s cubic-bezier(0.550, 0.085, 0.680, 0.530) both";
@@ -36,11 +43,12 @@ menuButton.addEventListener("click", function () {
       });
     }, 400);
   } else {
-    menu.style.display = "block";
-    menu.style.animation = "menu-background-reveal 0.6s linear";
-    menu.style.backgroundColor = "#fff";
-    menu.style.width = "190px";
-    //Recolhe os itens do menu
+    //Altera o menu para mobile
+    menuMobile.style.display = "block";
+    menuMobile.style.animation = "menu-background-reveal 0.6s linear";
+    menuMobile.style.backgroundColor = "#fff";
+    menuMobile.style.width = "190px";
+    //Mostra os itens do menu mobile
     menuItem.forEach((item) => {
       item.style.animation =
         "menu-item-tracking-in 0.5s cubic-bezier(0.215, 0.610, 0.355, 1.000) 0.3s both";
@@ -60,3 +68,19 @@ menuButton.addEventListener("click", function () {
     }, 400);
   }
 });
+
+//Corrige o bug que quebrava a navbar ao abrir o menu-burguer em mobile e rotacionar a tela para desktop
+function fixMenuMobileDisplay() {
+  navWidth = window.innerWidth;
+  console.log(typeof navWidth + "\n" + navWidth);
+  if (navWidth > 904) {
+    menuMobile.style.display = "none";
+    buttonMenuSvgClosing.style.display = "block";
+    buttonMenuSvgOpen.style.display = "none";
+    menuMobileRect.forEach((item) => {
+      item.style.animation = "none";
+    });
+  }
+}
+
+window.addEventListener("resize", fixMenuMobileDisplay);
