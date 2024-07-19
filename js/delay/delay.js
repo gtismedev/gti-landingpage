0cd48a7a71c18c638c49658291e404437b7d04a8
FellipeMiguel <fellipemiguel1@icloud.com>
2024-07-19 14:00:40 +0200

    feat: adicionando delay aos conteudos das seção

---
+++ js/delay/delay.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+  //Hero
+  setTimeout(function () {
+    const heroElement = document.getElementById("menu__hero");
+    const aboutUsElement = document.getElementById("about-us");
+
+    heroElement.style.display = "flex";
+    heroElement.style.opacity = 1;
+    heroElement.style.transition = "opacity 2s ease";
+
+    aboutUsElement.style.opacity = 1;
+    aboutUsElement.style.transition = "opacity 2s ease";
+  }, 500);
+});
